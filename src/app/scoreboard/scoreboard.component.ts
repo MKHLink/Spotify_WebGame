d92abb3eedaba1043c2e7a98142f382b67f365e4
PhamJimmy <JimmyPhamRes@gmail.com>
2023-12-19 15:02:56 -0800

    scoreboard refactor

--- src/app/scoreboard/scoreboard.component.ts
+++ src/app/scoreboard/scoreboard.component.ts
@@ -4,32 +4,34 @@ import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-scoreboard',
   templateUrl: './scoreboard.component.html',
-  styleUrls: ['./scoreboard.component.css']
+  styleUrls: ['./scoreboard.component.css'],
 })
 export class ScoreboardComponent implements OnInit {
+  scoreboard: any[] = [];
 
-  fakeScoreboard: any[] = [];
+  topTenScores: any[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.loadFakeScoreboard();
+    this.loadScoreboard();
+    this.topTenScores = this.scoreboard.slice(0, 10);
   }
 
-  private loadFakeScoreboard(): void {
-    const storedData = localStorage.getItem('fakeScoreboard');
+  private loadScoreboard(): void {
+    const storedData = localStorage.getItem('scoreboard');
 
     if (storedData) {
-      this.fakeScoreboard = JSON.parse(storedData);
+      this.scoreboard = JSON.parse(storedData);
     }
 
-    if (this.fakeScoreboard.length < 10) {
-      for (let i = this.fakeScoreboard.length; i < 10; i++) {
+    if (this.scoreboard.length < 10) {
+      for (let i = this.scoreboard.length; i < 10; i++) {
         const fakeData = {
           username: faker.internet.userName(),
-          score: faker.number.int({ min: 1, max: 9000 })
+          score: faker.number.int({ min: 1, max: 9000 }),
         };
-        this.fakeScoreboard.push(fakeData);
+        this.scoreboard.push(fakeData);
       }
 
       this.sortAndSaveScoreboard();
@@ -38,26 +40,19 @@ export class ScoreboardComponent implements OnInit {
 
   //function to sort data by score before saving
   sortAndSaveScoreboard(): void {
-    this.fakeScoreboard.sort((a, b) => b.score - a.score);
-    localStorage.setItem('fakeScoreboard', JSON.stringify(this.fakeScoreboard));
+    this.scoreboard.sort((a, b) => b.score - a.score);
+    localStorage.setItem('scoreboard', JSON.stringify(this.scoreboard));
   }
 
   //function to add a new entry via other components, this function checks if the new entry's scroe is high enough to earn a place in the scoreboard
   addNewEntry(username: string, score: number): void {
     const newEntry = {
       username: username,
-      score: score
+      score: score,
     };
 
-    const lowerScore = this.fakeScoreboard.find(entry=>entry.score<score);
-
-    if(lowerScore){
-      const i = this.fakeScoreboard.indexOf(lowerScore);
-      this.fakeScoreboard[i] = newEntry;
-      this.sortAndSaveScoreboard();
-    }else if(this.fakeScoreboard.length<10){
-      this.fakeScoreboard.push(newEntry);
-      this.sortAndSaveScoreboard();
-    } 
+    this.scoreboard.push(newEntry);
+    this.sortAndSaveScoreboard();
+    this.topTenScores = this.scoreboard.slice(0, 10);
   }
 }
