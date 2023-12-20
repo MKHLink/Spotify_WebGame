8e14e109d1efd9fcb793a9bce1e91d1edd87e345
MKHLink <likhonhasankomol@gmail.com>
2023-12-20 15:47:55 -0500

    able to display current track info and use the different api beased on settings

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { random } from 'lodash';
 // import fetchFromSpotify, { request } from '../../services/api';
 import { fetchToken, getRandomSongs, searchSpotifyByGenre, searchSpotifyByArtist } from 'src/services/api';
+import { Howl, Howler } from 'howler';
 
 const TOKEN_KEY = 'whos-who-access-token';
 
@@ -14,12 +16,20 @@ export class HomeComponent implements OnInit {
 
   username: string = '';
 
+  songs: any [] =[];
+  currentSongIndex: number = 0;
+  sound:any;
+  showPlayer: boolean = false;
+  isCompletelyRandom: any;
+  isBasedOnGenre: any;
+  isBasedOnArtist: any;
+  searchString: any;
+
   constructor() {}
 
   ngOnInit(): void {
     const tokenKey = localStorage.getItem(TOKEN_KEY);
-    console.log('tokenKey: ', tokenKey);
-
+  
     if (tokenKey) {
       const token = JSON.parse(tokenKey);
       if (token.expiration > Date.now()) {
@@ -31,27 +41,107 @@ export class HomeComponent implements OnInit {
     } else {
       this.getSpotifyAuthToken();
     }
-
-    getRandomSongs(this.token).then((response: any) => {
-      console.log('Random songs: ', response);
-    });
-
-    searchSpotifyByArtist(this.token, 'The Beatles').then((response: any) => {
-      console.log('The Beatles search results: ', response);
-    });
-
-    searchSpotifyByGenre(this.token, 'jazz').then((response: any) => {
-      console.log('Jazz search results: ', response);
-    });
+  
+    // Fetch settings from localStorage
+    const settings = localStorage.getItem('spotify-pop-quiz-settings');
+    if (settings) {
+      const parsedSettings = JSON.parse(settings);
+      this.isCompletelyRandom = parsedSettings.isCompletelyRandom;
+      this.isBasedOnGenre = parsedSettings.isBasedOnGenre;
+      this.isBasedOnArtist = parsedSettings.isBasedOnArtist;
+      this.searchString = parsedSettings.searchString;
+    }
+  
+    // Fetch songs based on settings
+    this.fetchSongs();
   }
-
+  
+  fetchSongs(): void {
+    if (this.token) {
+      if (this.isCompletelyRandom) {
+        getRandomSongs(this.token).then((response: any) => {
+          console.log('Random songs: ', response);
+          this.songs = response.tracks.items.filter((track: any) => track.preview_url);
+          console.log(this.songs);
+        });
+      } else if (this.isBasedOnArtist) {
+        searchSpotifyByArtist(this.token, this.searchString).then((response: any) => {
+          console.log('Artist search results: ', response);
+          this.songs = response.tracks.items.filter((track: any) => track.preview_url);
+          console.log(this.songs);
+        });
+      } else if (this.isBasedOnGenre) {
+        searchSpotifyByGenre(this.token, this.searchString).then((response: any) => {
+          console.log('Genre search results: ', response);
+          this.songs = response.tracks.items.filter((track: any) => track.preview_url);
+          console.log(this.songs);
+        });
+      }
+    }
+  }
+  
+ 
   receiveUsername = (valueEmitted: string) => {
     this.username = valueEmitted;
   };
 
   submitUsername = () => {
     console.log('Username: ', this.username);
+    this.showPlayer = true;
+    this.initializeSound();
   };
+
+  initializeSound(): void {
+    const previewUrl = this.songs[this.currentSongIndex]?.preview_url;
+    if (previewUrl) {
+      this.sound = new Howl({
+        src: [previewUrl],
+        html5: true,
+        onplay: () => {
+          console.log('Playing');
+        }
+      });
+      this.play();
+    }
+  }
+
+  getCurrentSongAlbumCover(): string {
+    return this.songs[this.currentSongIndex]?.album?.images[0]?.url || 'default-cover.jpg';
+  }
+
+  getCurrentSongName(): string {
+    return this.songs[this.currentSongIndex]?.name || 'Unknown Song';
+  }
+
+  getCurrentSongArtist(): string {
+    return this.songs[this.currentSongIndex]?.artists[0]?.name || 'Unknown Artist';
+  }
+
+  nextSong = () => {
+    if (this.sound) {
+      this.sound.stop();
+    }
+
+    if (this.currentSongIndex < this.songs.length - 1) {
+      this.currentSongIndex++;
+    } else {
+      this.currentSongIndex = 0;
+    }
+
+    this.initializeSound();
+  };
+
+  play(): void {
+    if (this.sound && !this.sound.playing()) {
+      this.sound.play();
+    }
+  }
+
+  pause(): void {
+    if (this.sound && this.sound.playing()) {
+      this.sound.pause();
+    }
+  }
 
   getSpotifyAuthToken = () => {
     fetchToken().then(({ access_token, expires_in }: { access_token: string; expires_in: number }) => {
@@ -64,3 +154,4 @@ export class HomeComponent implements OnInit {
     });
   };
 }
+
